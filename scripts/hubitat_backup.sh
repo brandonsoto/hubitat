8dab59e0d6d24347e04b42798bb4e69cfbc22a70
Brandon Soto <brandon.soto09@gmail.com>
2020-02-16 14:57:12 -0800

    Use cookie when downloading backups

--- scripts/hubitat_backup.sh
+++ scripts/hubitat_backup.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 # Script to download hubitat backup
-# https://community.hubitat.com/t/solved-downloading-latest-backup-file/18065/12
 
-cd [DIRECTORY_OF_YOUR_BACKUPS]
-ls -1tr | head -n -3 | xargs -d '\n' rm -f --
-curl http://[HUBIPHERE]/hub/backup | grep data-fileName | grep download | sed 's/<td class=\"mdl-data-table__cell--non-numeric\"><a class=\"download mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect\" href=\"#\"  data-fileName=\"/\ /' | sed 's/\">Download<\/a><\/td>/\ /' | sed 's/ //g' | tail -1 | xargs -I @ curl -o @ http://[HUBIPHERE]/hub/backupDB?fileName=@
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# Required environment variables:
+#   - HUBITAT_BACKUP_DIR
+#   - HUBITAT_LOGIN
+#   - HUBITAT_PW
+#   - HUBITAT_ADDR
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+cd $HUBITAT_BACKUP_DIR
+export COOKIE_FILE="/tmp/hubitat_cookie"
+export MAX_HUBITAT_BACKUPS=5
+
+
+curl -k -c $COOKIE_FILE -d username=$HUBITAT_LOGIN -d password=$HUBITAT_PW $HUBITAT_ADDR/login
+
+ls -1tr | head -n -$MAX_HUBITAT_BACKUPS | xargs -d '\n' rm -f --
+
+curl -s -X GET -k -b $COOKIE_FILE $HUBITAT_ADDR/hub/backup | grep data-fileName | grep download | sed 's/<td class=\"mdl-data-table__cell--non-numeric\"><a class=\"download mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect\" href=\"#\"  data-fileName=\"/\ /' | sed 's/\">Download<\/a><\/td>/\ /' | sed 's/ //g' | tail -1 | xargs -I @ curl -s -X GET -k -b $COOKIE_FILE -o @ $HUBITAT_ADDR/hub/backupDB?fileName=@
